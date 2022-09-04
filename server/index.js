cb412ade8b97ea6237aff120840a17910025f809
Sunil Kumar <sunilk10099@gmail.com>
2022-09-05 02:07:08 +0530

    Basic port setup and response

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+//midlewares
+app.use(express.json());
+
+//server status
+app.get("/api/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "Server is up and running.",
+  });
+});
+
+//request does not match any route
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Resource not found",
+  });
+});
+
+//listening to port
+app.listen(process.env.PORT || 3500, () => {
+  console.log(`server running at port ${process.env.PORT}`);
+});
