6ce6e744d72fc303c9a2686912e351772e57d60f
Sunil Kumar <sunilk10099@gmail.com>
2022-09-07 00:50:15 +0530

    add auth routes

---
+++ server/Routes/Auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+//login user
+router.post("/login");
+
+//sign up user
+router.post("/signup");
+
+//forgot password
+router.post("/forgotpassword");
