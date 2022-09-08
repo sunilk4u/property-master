0bc50ec89b19c034582bd8b421b60fd3b55964e6
Sunil Kumar <sunilk10099@gmail.com>
2022-09-08 20:11:12 +0530

    Add field validation

--- server/Controller/Auth.js
+++ server/Controller/Auth.js
@@ -4,12 +4,28 @@ const ErrorHandler = require("../Utils/ErrorHandler");
 const registerUser = (req, res) => {
   const { name, email, password, role } = req.body;
 
+  //field validation
+  if (name.length < 4) {
+    return ErrorHandler(
+      req,
+      res,
+      400,
+      "Name length should be greater than 3 characters"
+    );
+  } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return ErrorHandler(req, res, 400, "Invalid Email");
+  } else if (password.length < 8 || !/\d/.test(password)) {
+    return ErrorHandler(req, res, 400, "Weak Password");
+  } else if (role === null || role === "") {
+    return ErrorHandler(req, res, 400, "Role field is empty");
+  }
+
   //check if user is already present
   User.findOne({ email: email }, (err, user) => {
     if (user) {
-      ErrorHandler(req, res, 400, "User is already registered");
+      return ErrorHandler(req, res, 400, "User is already registered");
     } else if (err) {
-      ErrorHandler(req, res, 500, err.message);
+      return ErrorHandler(req, res, 500, err.message);
     } else {
       const user = new User({
         name,
@@ -17,9 +33,10 @@ const registerUser = (req, res) => {
         password,
         role,
       });
+      //if user is not preset then save it
       user.save((err) => {
         if (err) {
-          ErrorHandler(req, res, 500, err.message);
+          return ErrorHandler(req, res, 500, err.message);
         } else {
           res.status(201).json({
             name: user.name,
