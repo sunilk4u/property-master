5dc5bfbf14827d1c091defd734c8bd0a357be556
Sunil Kumar <sunilk10099@gmail.com>
2022-09-08 19:33:33 +0530

    add mongo connection options

--- server/Config/db.js
+++ server/Config/db.js
@@ -3,12 +3,20 @@ const mongoose = require("mongoose");
 const mongoUrl = process.env.MONGO_URI;
 
 //create connection to mongodb atlas
-const db_connect = () => mongoose.connect(mongoUrl, (err) => {
-  if (err) {
-    console.log("Error occured connecting to database.")
-    throw err;
-  }
-  console.log("mongo database connected.");
-})
+const db_connect = () =>
+  mongoose.connect(
+    mongoUrl,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) {
+        console.log("Error occured connecting to database.");
+        throw err;
+      }
+      console.log("mongo database connected.");
+    }
+  );
 
 module.exports = db_connect;
