6b27ed7ff107eacde21f0f52695ac8253d033fb0
Sunil Kumar <sunilk10099@gmail.com>
2022-09-14 18:51:11 +0530

    Add header component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import { Container, Typography, Button, Toolbar } from "@mui/material";
+import { AppBarSx, ContainerSx, LogInButtonSx, SignUpButtonSx, TypographySx } from "./styles";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <AppBar position="static" sx={AppBarSx}>
+        <Container maxWidth="xl" sx={ContainerSx}>
+          <Toolbar>
+            <Typography variant="h4" sx={TypographySx} component="a">
+              Property Master
+            </Typography>
+            <Button variant="contained" sx={LogInButtonSx}>Log in</Button>
+            <Button variant="outlined" sx={SignUpButtonSx}>Sign Up</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/components/Header/styles.jsx
@@ -0,0 +1,21 @@
+export const AppBarSx = {
+  backgroundColor: "#3D2C8D",
+  height: "4rem",
+};
+
+export const ContainerSx = {
+  my: "auto",
+};
+
+export const TypographySx = {
+  fontWeight: 800,
+  flexGrow: "1",
+};
+export const LogInButtonSx = {
+  mx: 1,
+  backgroundColor: "#C996CC",
+};
+export const SignUpButtonSx = {
+  mx: 1,
+  color: "#fff",
+};
